110de8328591c1602bc7efb5477a35f227c6d7d0
Anthony Grimes <i@raynes.me>
2012-06-23 22:02:46 -0500

    Upgrade to lein 2 and get rid of multi-deps in favor of lein 2's profile features.

--- project.clj
+++ project.clj
@@ -1,16 +1,11 @@
-;; common dependencies:
-(def deps '[[org.clojure/core.incubator "0.1.0"]
-            [org.clojure/data.json "0.1.3"]
-            [org.mongodb/mongo-java-driver "2.7.3"]])
-
-;; project definition for multi-version testing:
-(defproject congomongo
-  "0.1.10-SNAPSHOT"
+(defproject congomongo "0.1.10-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
-  :dev-dependencies [[lein-multi "1.1.0"]
-                     [swank-clojure "1.4.2"]]
   :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-  :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
-  :multi-deps {"1.2"  ~(conj deps '[org.clojure/clojure "1.2.1"])
-               "1.4"  ~(conj deps '[org.clojure/clojure "1.4.0"])
-               "1.5S" ~(conj deps '[org.clojure/clojure "1.5.0-master-SNAPSHOT"])})
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.incubator "0.1.0"]
+                 [org.clojure/data.json "0.1.3"]
+                 [org.mongodb/mongo-java-driver "2.7.3"]]
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.4,default:dev,1.5,default" "test"]}
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}})
